d028993f181a19bb2d1b05dc5ea2b621d14d4759
Steve <mittosteve@gmail.com>
2019-08-30 17:15:06 +0300

    This commit adds a confirm password to the constractor

--- user.py
+++ user.py
@@ -4,7 +4,7 @@ class Account:
     This is the main class for the users accounts
     '''
     accounts_list = []
-    def __init__(self,first_name,last_name,email,password):
+    def __init__(self,first_name,last_name,email,password,c_password):
         '''
         This function is the blue print of users accounts
         '''
@@ -12,6 +12,7 @@ class Account:
         self.last_name = last_name
         self.email = email
         self.password = password
+        self.c_password = c_password
 
     def save_account(self):
         '''
--- user_test.py
+++ user_test.py
@@ -7,7 +7,7 @@ class TestUser (unit.TestCase):
         '''
         This function creates a default user setUp
         '''
-        self.new_account = ac("John","Doe","john@gmail.com","12johnDoes34",)
+        self.new_account = ac("John","Doe","john@gmail.com","12johnDoes34","12johnDoes34")
 
     def tearDown(self):
         '''
@@ -23,6 +23,7 @@ class TestUser (unit.TestCase):
         self.assertEqual(self.new_account.last_name,"Doe")
         self.assertEqual(self.new_account.email,"john@gmail.com")
         self.assertEqual(self.new_account.password,"12johnDoes34")
+        self.assertEqual(self.new_account.c_password,self.new_account.password)
 
     def test_save_account(self):
         '''
@@ -36,8 +37,10 @@ class TestUser (unit.TestCase):
         This function test whether a user can save many accounts
         '''
         self.new_account.save_account()
-        account_2 =ac ("Some","one","some@gmail.com","12someOne34",)
+        account_2 =ac ("Some","one","some@gmail.com","12someOne34","12someOne34")
         account_2.save_account()
         self.assertEqual(len(ac.accounts_list),2)
+
+
 if __name__ == '__main__':
     unit.main()
