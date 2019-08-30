68d63a7f9bb6806f937abd10c7465ee1c86976b5
Steve <mittosteve@gmail.com>
2019-08-30 16:14:40 +0300

    This commit makes sure the initialization test is passed

---
+++ user.py
@@ -0,0 +1,12 @@
+
+class account:
+    '''
+    This is the main class for the users accounts
+    '''
+
+    def __init__(self,first_name,last_name,email,password):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
--- user_test.py
+++ user_test.py
@@ -1,4 +1,4 @@
-import user as us
+from user import account as ac
 import unittest as unit
 
 class TestUser (unit.TestCase):
@@ -7,4 +7,18 @@ class TestUser (unit.TestCase):
         '''
         This function creates a default user setUp
         '''
-        us.account("John","Doe","john@gmail.com","12johnDoes34",)
+        self.new_account = ac("John","Doe","john@gmail.com","12johnDoes34",)
+
+    def test_init(self):
+        '''
+        This function tests whether the users account has been initialized
+        '''
+
+        self.assertEqual(self.new_account.first_name,"John")
+        self.assertEqual(self.new_account.last_name,"Doe")
+        self.assertEqual(self.new_account.email,"john@gmail.com")
+        self.assertEqual(self.new_account.password,"12johnDoes34")
+
+
+if __name__ == '__main__':
+    unit.main()
