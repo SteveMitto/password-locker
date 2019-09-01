d81cd621866c9a02591252ca71aaf789ee90e27e
Steve <mittosteve@gmail.com>
2019-09-01 23:19:17 +0300

    This commit allows users to save their cridentials for instagram

--- run_py.py
+++ run_py.py
@@ -1,6 +1,6 @@
 from cridentials import cridentials as cri
 from user import Account as acc
-
+import getpass
 def create_account(fname,lname,email,password,c_password):
     '''
     This function allows creation of an account
@@ -14,6 +14,20 @@ def save_account(account):
     '''
     account.save_account()
 
+def create_cridentials(username,email,password):
+    '''
+    This function allows users to save a new cridentials
+    '''
+    new_cridentials = cri(username,email,password)
+    return new_cridentials
+
+def save_cridentials(cridential,app):
+    '''
+    This commit allows users to save their cridentials
+    '''
+    cridential.save_cridentials(app)
+
+
 def main():
     '''
     This is the body of the function
@@ -26,15 +40,29 @@ def main():
             fname = input("... First Name ...\n")
             lname = input("... Last Name ...\n")
             email = input("... Email ...\n")
-            password = input("... Password ...\n")
-            c_password = input("... Confirm Password ...\n")
+            password = getpass.getpass("... Password ...\n")
+            c_password = getpass.getpass("... Confirm Password ...\n")
             while password != c_password:
                 print("Passwords don't match. \n Please confirm password")
                 c_password = input("... Confirm Password ...\n")
             else:
                 save_account(create_account(fname,lname,email,password,c_password))
                 print("Hey "+acc.accounts_list[0].first_name + " your account was created")
-
+                while True :
+                    print(f"""Welcome {acc.accounts_list[0].first_name} please select the account cridentials that you want to fill by using the following keys
+                ig - Instagram          twi - Twitter          fb - Facebook
+                li - Linked In          snap - SnapChat        gm - Gmail
+                                        yh - Yahoo
+                    """)
+                    res = input()
+                    if res == 'ig':
+                        app="instagram"
+                        username = input(" ... Username ... \n")
+                        email = input(" ... Email ...\n")
+                        # pwd = input("Do you want a generated password or not\n (y/n)")
+                        password =getpass.getpass(" ... Password ...\n")
+                        save_cridentials(create_cridentials(username,email,password),app)
+                        print(cri.social_accounts["instagram"][0].password)
         else:
             print("________________________________invalid responce________________________________")
 if __name__ == "__main__":
